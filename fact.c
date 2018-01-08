727b2bce6a36cb7d8b0d1460ae0cbbfb97eb76ba
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-01-08 03:30:31 -0800

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,14 @@
+#include<conio.h>
+int main()
+{
+ int a,i,fact=1;
+ printf("/n Enter the number");
+ scanf("%d",&a);
+ for(i=0;i<=a;i++)
+  {
+   fact=fact*i;
+   }
+   printf("/n the factorial is %d",fact);
+  return 0;
+  }
+  
