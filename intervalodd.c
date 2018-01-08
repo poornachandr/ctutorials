a2eea85a5c9f92d1cecf0e1a0204ea5d92cb5a5a
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-01-08 02:59:53 -0800

    Create intervalodd.c

---
+++ intervalodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,i;
+ printf("/n enter the first number");
+ scanf("%d",&a);
+ scanf("/n enter the second number");
+ scanf("%d",&b);
+ for(i=a;i<=b,i++)
+ {
+  if(i%2!=0)
+   {
+    printf("/n the odd numbers are %d",i);
+   }
+  }
+  return o;
+ } 
