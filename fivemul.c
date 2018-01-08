8e38a2c46ba7c3ccbd6d71eea7d09590c5278515
poornachandr <31505561+poornachandr@users.noreply.github.com>
2018-01-08 03:22:15 -0800

    Create fivemul.c

---
+++ fivemul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+ int a,n,i;
+ printf("/n ENTER A NUMBER");
+ scanf("%d",&a);
+ for(i=0;i<=5;i++)
+ {
+  printf("the first five multiples are %d,a*i);
+ }
+ return 0;
+ }
